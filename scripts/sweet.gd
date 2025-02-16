6ef1bc6159bf06cfa5f9d88b511330b981ecf6aa
Anthi <55553311+AnthiNassi@users.noreply.github.com>
2025-02-17 01:11:21 +0200

    asset update

--- scripts/sweet.gd
+++ scripts/sweet.gd
@@ -2,13 +2,10 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $"./AnimationPlayer"
 @onready var sound_player: AudioStreamPlayer2D = $"./AudioStreamPlayer2D"
-@onready var sprite: Sprite2D = $"./Sprite2D"
 
-@export var image: Texture2D
 @export var sound_effect: AudioStream
 
 func _ready():
-	sprite.texture = image
 	sound_player.stream = sound_effect
 	
 func _on_body_entered(_body: Node2D) -> void:
