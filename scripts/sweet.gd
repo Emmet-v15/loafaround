a757ffb0fe0e208ae495cd36c15252e24173428e
Emmet_v15 <emmet@v15.studio>
2025-02-17 02:06:09 +0000

    Baguette time

--- scripts/sweet.gd
+++ scripts/sweet.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sound_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var animation_player: AnimationPlayer = $"./AnimationPlayer"
+@onready var sound_player: AudioStreamPlayer2D = $"./AudioStreamPlayer2D"
+@onready var sprite: Sprite2D = $"./Sprite2D"
 
+@export var image: Texture2D
 @export var sound_effect: AudioStream
 
 func _ready():
-	if GameManager.has_food.has(name):
-		queue_free()
+	sprite.texture = image
 	sound_player.stream = sound_effect
 	
 func _on_body_entered(_body: Node2D) -> void:
 	GameManager.add_point()
-	GameManager.has_food[name] = true		
 	animation_player.play("pickup")
