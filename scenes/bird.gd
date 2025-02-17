78bfd79d0393d0ba907fc91aaf7a9273f8c537c0
Emmet_v15 <emmet@v15.studio>
2025-02-17 02:13:16 +0000

    Update bird.gd

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 func _on_body_entered(_body: Node2D) -> void:
-	GameManager.has_baguette = true
-	get_parent().get_parent().get_node("AnimationPlayer").play("end")
-	print("baguette")
+	if GameManager.cutscene_played:
+		GameManager.has_baguette = true
+		get_parent().get_parent().get_node("AnimationPlayer").play("end")
+		
+		print("baguette")
